ceab0ae86cd4842d58884395b9e59d99f8ee6e9b
Corey Donohoe <atmos@atmos.org>
2009-03-01 14:34:26 -0700

    flesh out unit tests for openid mode == immediate

--- spec/acceptance/openid/handshake_spec.rb
+++ spec/acceptance/openid/handshake_spec.rb
@@ -106,4 +106,80 @@ describe "visiting /openid" do
       end
     end
   end
+  describe "checkid_immediate" do
+    describe "unauthenticated user" do
+      it "should require authentication" do
+        params = {
+          "openid.ns"         => "http://specs.openid.net/auth/2.0",
+          "openid.mode"       => "checkid_immediate",
+          "openid.return_to"  => @consumer.url,
+          "openid.identity"   => "http://example.org/users/#{@user.id}",
+          "openid.claimed_id" => "http://example.org/users/#{@user.id}"}
+
+        get "/openid", params
+        @response.should have_selector("form[action='/users/login'][method='POST']")
+        @response.should have_selector("form[action='/users/login'][method='POST'] input[type='text'][name='login']")
+        @response.should have_selector("form[action='/users/login'][method='POST'] input[type='password'][name='password']")
+        @response.should have_selector("form[action='/users/login'][method='POST'] input[type='submit'][value='Login']")
+      end
+    end
+    describe "authenticated user" do
+      describe "with appropriate request parameters" do
+        it "should redirect to the consumer app" do
+          params = {
+            "openid.ns"         => "http://specs.openid.net/auth/2.0",
+            "openid.mode"       => "checkid_immediate",
+            "openid.return_to"  => @consumer.url,
+            "openid.identity"   => "http://example.org/users/#{@user.id}",
+            "openid.claimed_id" => "http://example.org/users/#{@user.id}"}
+
+          get "/openid", params, :session => {:user_id => @user.id}
+          @response.status.should == 302
+
+          redirect_params = Addressable::URI.parse(@response.headers['Location']).query_values
+
+          redirect_params['openid.ns'].should               == 'http://specs.openid.net/auth/2.0'
+          redirect_params['openid.mode'].should             == 'id_res'
+          redirect_params['openid.return_to'].should        == @consumer.url
+          redirect_params['openid.assoc_handle'].should     =~ /^\{HMAC-SHA1\}\{[^\}]{8}\}\{[^\}]{8}\}$/
+          redirect_params['openid.op_endpoint'].should      == 'http://example.org/openid' 
+          redirect_params['openid.claimed_id'].should       == "http://example.org/users/#{@user.id}"
+          redirect_params['openid.identity'].should         == "http://example.org/users/#{@user.id}"
+
+          redirect_params['openid.sig'].should_not be_nil
+          redirect_params['openid.signed'].should_not be_nil
+          redirect_params['openid.response_nonce'].should_not be_nil
+        end
+      end
+
+      describe "attempting to access another identity" do
+        it "should return forbidden" do
+          params = {
+            "openid.ns"         => "http://specs.openid.net/auth/2.0",
+            "openid.mode"       => "checkid_immediate",
+            "openid.return_to"  => @consumer.url,
+            "openid.identity"   => "http://example.org/users/42",
+            "openid.claimed_id" => "http://example.org/users/42" }
+
+
+          get "/openid", params, :session => {:user_id => @user.id}
+          @response.status.should == 403
+        end
+      end
+
+      describe "attempting to access from an untrusted consumer" do
+        it "cancel the openid request" do
+          params = {
+            "openid.ns"         => "http://specs.openid.net/auth/2.0",
+            "openid.mode"       => "checkid_immediate",
+            "openid.return_to"  => "http://rogueconsumerapp.com/",
+            "openid.identity"   => "http://example.org/users/#{@user.id}",
+            "openid.claimed_id" => "http://example.org/users/#{@user.id}"}
+
+          get "/openid", params, :session => {:user_id => @user.id}
+          response.status.should == 403
+        end
+      end
+    end
+  end
 end
