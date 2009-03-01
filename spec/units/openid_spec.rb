a61d679e0e2726bbe554ec71cb8ec0518bfa52eb
Corey Donohoe <atmos@atmos.org>
2009-03-01 14:37:01 -0700

    the openid spec is more of a unit test

--- spec/units/openid_spec.rb
+++ spec/units/openid_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__)+'/../../spec_helper')
+require File.expand_path(File.dirname(__FILE__)+'/../spec_helper')
 
 describe "visiting /openid" do
   before(:each) do
