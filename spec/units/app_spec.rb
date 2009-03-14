fef6557e4504e8013f84dc1644d5963d71779229
Corey Donohoe <atmos@atmos.org>
2009-03-14 15:39:38 -0600

    add specs for the sreg configuration options code

---
+++ spec/units/app_spec.rb
@@ -0,0 +1,15 @@
+require File.expand_path(File.dirname(__FILE__)+'/../spec_helper')
+
+describe Hancock::App do
+  describe "default sreg parameters" do
+    it "returns sane defaults" do
+      Hancock::App.sreg_params.should eql([:email, :first_name, :last_name, :internal])
+    end
+  end
+  describe "setting the default sreg parameters" do
+    it "returns the values set by the method" do
+      Hancock::App.sreg_params = [:email, :birthdate, :gender]
+      Hancock::App.sreg_params.should eql([:email, :birthdate, :gender])
+    end
+  end
+end
